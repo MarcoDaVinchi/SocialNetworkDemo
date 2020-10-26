aa99f1e8e1203884b0f7794e76e826c1fa41997f
Marat Magulaev <mag-marat@yandex.ru>
2020-10-26 19:19:02 +0000

    Implementation of auth guard interceptor and fake backend

---
+++ src/app/auth/error.interceptor.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse,
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { AuthenticationService } from '../services/authentication.service';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorInterceptor implements HttpInterceptor {
+  constructor(private authenticationService: AuthenticationService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((err) => {
+        if (err instanceof HttpErrorResponse && err.status === 401) {
+          this.authenticationService.logout();
+          location.reload();
+        }
+
+        const error = err.error.message || err.statusText;
+        return throwError(error);
+      })
+    );
+  }
+}
